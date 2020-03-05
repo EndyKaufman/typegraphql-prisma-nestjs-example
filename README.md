ac29e76e372be70d97430f3e5d8d57a54ef9e6d3
ILshat Khamitov <admin@site15.ru>
2020-03-05 16:03:34 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# nest-typescript-starter
+
+Example use NestJS + Prisma2 + Typegraphql
+
+## Install
+```
+git clone git@github.com:EndyKaufman/typegraphql-prisma-nestjs.git
+cd typegraphql-prisma-nestjs
+npm i
+```
+
+## Run
+```
+npm run start
+```
+
+## Open browser
+http://localhost:3000/graphql - graphql gui
+
+## links
+https://github.com/nestjs/nest/tree/master/sample/23-type-graphql
+
+https://github.com/prisma/prisma2/blob/master/docs/getting-started/quickstart-existing-project.md
+
+https://github.com/MichalLytek/type-graphql
+
+https://github.com/EndyKaufman/typegraphql-prisma-nestjs
---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { PrismaClient } from "@prisma/client";
+import { CrudResolversModule, RelationsResolversModule } from "./dal";
+import { RecipesModule } from "./recipes/recipes.module";
+
+const prisma = new PrismaClient({
+  debug: true,
+  log: ["query"]
+});
+
+@Module({
+  imports: [
+    RecipesModule,
+    CrudResolversModule,
+    RelationsResolversModule,
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: "schema.gql",
+      debug: true,
+      playground: true,
+      tracing: true,
+      context: ({ req }) => ({ req, prisma })
+    })
+  ]
+})
+export class AppModule {}
