dac9bada3760ef58ee1e1810455a85d95fa3e556
EndyKaufman <admin@site15.ru>
2025-01-24 11:22:59 +0400

    chore: add config for deploy to vercel

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("ok"));
+
+app.listen(process.env.PORT, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
